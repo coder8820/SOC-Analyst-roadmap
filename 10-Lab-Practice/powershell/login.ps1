e5861d2c1a75421531c25289dda7ffe629f7b964
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-02-23 14:31:34 +0500

    changed the pid

--- 10-Lab-Practice/powershell/login.ps1
+++ 10-Lab-Practice/powershell/login.ps1
@@ -1,3 +1,3 @@
 # count failed login attempts(EventId 4625)
-$failed = Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4625
+$failed = Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4624
 Write-Output 'Total failed Login: $(failed.Count)'
